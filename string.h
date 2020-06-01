6535dafb11c992c5602b8268b456c4e0f17c8bd9
Thierry Supplisson <ts@js353.com>
2020-06-01 23:09:47 +0100

    Further alignment with https://github.com/agn453/HI-TECH-Z80-C

--- string.h
+++ string.h
@@ -16,31 +16,37 @@ typedef	unsigned	size_t;		/* type yielded by sizeof */
 
 extern int	errno;			/* system error number */
 
-extern void *	memcpy(void *, void *, size_t);
-extern void *	memmove(void *, void *, size_t);
-extern char *	strcpy(char *, char *);
-extern char *	strncpy(char *, char *, size_t);
-extern char *	strcat(char *, char *);
-extern char *	strncat(char *, char *, size_t);
-extern int	memcmp(void *, void *, size_t);
-extern int	strcmp(char *, char *);
-extern int	stricmp(char *, char *);
-extern int	strcasecmp(char *, char *);
-extern int	strncmp(char *, char *, size_t);
-extern int	strnicmp(char *, char *, size_t);
-extern int	strncasecmp(char *, char *, size_t);
-extern size_t	strcoll(char *, size_t, char *);
-extern void *	memchr(void *, int, size_t);
-extern size_t	strcspn(char *, char *);
-extern char *	strpbrk(char *, char *);
-extern size_t	strspn(char *, char *);
-extern char *	strstr(char *, char *);
-extern char *	strtok(char *, char *);
-extern void *	memset(void *, int, size_t);
-extern char *	strerror(int);
-extern size_t	strlen(char *);
-extern char *	strchr(char *, int);
-extern char *	strrchr(char *, int);
-extern char *   rindex(char *, int);
-extern char *   index(char *, int);
+extern void	*memcpy(void *, void *, size_t);
+extern void	*memmove(void *, void *, size_t);
+extern char	*strcpy(char *, char *);
+extern char	*strncpy(char *, char *, size_t);
+extern char	*strcat(char *, char *);
+extern char	*strncat(char *, char *, size_t);
+extern int	 memcmp(void *, void *, size_t);
+extern int	 strcmp(char *, char *);
+extern int	 strcasecmp(char *, char *);
+extern int	 stricmp(char *, char *);
+extern int	 strncmp(char *, char *, size_t);
+extern int	 strncasecmp(char *, char *, size_t);
+extern int	 strnicmp(char *, char *, size_t);
+/* extern size_t	 strcoll(char *, size_t, char *); */ /* missing */
+extern void	*memchr(void *, int, size_t);
+/* extern size_t	 strcspn(char *, char *); */ /* missing */
+/* extern char	*strpbrk(char *, char *); */ /* missing */
+/* extern size_t	 strspn(char *, char *); *//* missing */
+extern char	*strstr(char *, char *);
+extern char	*strtok(char *, char *);
+extern void	*memset(void *, int, size_t);
+extern char	*strerror(int);
+extern size_t	 strlen(char *);
+extern char	*strchr(char *, int);
+/* #define index	strchr */	/* these are equivalent */
+extern char	*index(char *, int);
+extern char	*strrchr(char *, int);
+/* #define	rindex	*strrchr */	/* these are equivalent */
+extern char	*rindex(char *, int);
+extern char	*strcasestr(char *, char *);
+#define stristr	strcasestr
+extern char	*strncasestr(char *, char *, size_t);
+#define	strnistr strncasestr
 #endif
